1148b832df1733f2e0e1551136e0c7a05836c974
James Hamlin <james@goforward.com>
2022-10-01 18:28:38 -0700

    Add gain support back
    Signed-off-by: James Hamlin <james@goforward.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,38 @@ import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {
   SetGain,
-  GetNotes,
   RegisterWaveformCallback,
 } from "../wailsjs/go/main/App";
 
 function App() {
+  const handleGainChange = (gain) => {
+    SetGain(gain);
+  };
+
   return (
     <div id="App">
       <h2>Synthesizer</h2>
+      <FloatInput onValueChange={handleGainChange} />
     </div>
   )
+}
+
+function FloatInput(props: {onValueChange: (value: number) => void}) {
+  const [value, setValue] = useState(0.5);
+
+  const handleValueChange = (event: any) => {
+    setValue(Number(event.target.value))
+  };
+
+  useEffect(() => {
+    props.onValueChange(value);
+  }, [value]);
+
+  return (
+    <div className="input-box">
+      <input type="number" value={value} step="0.02" onChange={handleValueChange} />
+    </div>
+  );
 }
 
 export default App
