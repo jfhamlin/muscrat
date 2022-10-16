28b0600c641ae0805bc08279da9bc960fa1acac6
James Hamlin <james@goforward.com>
2022-10-16 15:38:01 -0700

    Add lores lowpass filter with resonance
    Signed-off-by: James Hamlin <james@goforward.com>

--- internal/pkg/mratlang/builtins.go
+++ internal/pkg/mratlang/builtins.go
@@ -81,6 +81,7 @@ func init() {
 				funcSymbol("delay", delayBuiltin),
 				funcSymbol("mixer", mixerBuiltin),
 				funcSymbol("env", envBuiltin),
+				funcSymbol("~lores", loresBuiltin),
 			},
 		},
 	}
@@ -1065,6 +1066,88 @@ func envBuiltin(env value.Environment, args []value.Value) (value.Value, error) 
 	for i, gen := range durationGens {
 		env.Graph().AddEdge(gen.NodeID, nodeID, fmt.Sprintf("time$%v", i))
 	}
+
+	return &value.Gen{
+		NodeID: nodeID,
+	}, nil
+}
+
+func NewLowpassFilterGenerator() generator.SampleGenerator {
+	// Translated from the SuperCollider extension source code here, which in turn mimics the
+	// max/msp lores~ object:
+	// https://github.com/v7b1/vb_UGens/blob/fea1587dd2165457c4a016214d17216987b56f00/projects/vbUtils/vbUtils.cpp
+	var a1, a2, fqterm, resterm, scale, ym1, ym2 float64
+	lastCut, lastRes := -1.0, -1.0
+	return generator.SampleGeneratorFunc(func(ctx context.Context, cfg generator.SampleConfig, n int) []float64 {
+		in := cfg.InputSamples["in"]
+		cuts := cfg.InputSamples["cutoff"]
+		ress := cfg.InputSamples["resonance"]
+		out := make([]float64, n)
+
+		for i := 0; i < n; i++ {
+			cut := cuts[i]
+			res := ress[i]
+			// clamp resonance to [0, 1)
+			if res < 0 {
+				res = 0
+			} else if res >= 1 {
+				res = 1.0 - 1e-20
+			}
+
+			if cut != lastCut || res != lastRes {
+				if res != lastRes {
+					resterm = math.Exp(res*0.125) * 0.882497
+				}
+				if cut != lastCut {
+					fqterm = math.Cos(cut * math.Pi * 2 / float64(cfg.SampleRateHz))
+				}
+				// recalculate the coefficients.
+				a1 = -2 * resterm * fqterm
+				a2 = resterm * resterm
+				scale = 1 + a1 + a2
+				lastCut, lastRes = cut, res
+			}
+			val := in[i]
+			temp := ym1
+			ym1 = scale*val - a1*ym1 - a2*ym2
+			ym2 = temp
+			out[i] = ym1
+		}
+		return out
+	})
+}
+
+func loresBuiltin(env value.Environment, args []value.Value) (value.Value, error) {
+	// lores takes three arguments:
+	//
+	// 1. a signal to be low-pass filtered.
+	//
+	// 2. a cutoff frequency, in Hz.
+	//
+	// 3. a resonance value, from 0 to 1.
+
+	if len(args) != 3 {
+		return nil, fmt.Errorf("~lores expects 3 arguments, got %v", len(args))
+	}
+
+	gen, ok := args[0].(*value.Gen)
+	if !ok {
+		return nil, fmt.Errorf("~lores expects a Gen as the first argument, got %v", args[0])
+	}
+
+	cutoff, ok := asGen(env, args[1])
+	if !ok {
+		return nil, fmt.Errorf("~lores expects a gennable as the second argument, got %v", args[1])
+	}
+	resonance, ok := asGen(env, args[2])
+	if !ok {
+		return nil, fmt.Errorf("~lores expects a gennable as the third argument, got %v", args[2])
+	}
+
+	nodeID := env.Graph().AddGeneratorNode(NewLowpassFilterGenerator(), graph.WithLabel("~lores"))
+	env.Graph().AddEdge(gen.NodeID, nodeID, "in")
+	env.Graph().AddEdge(cutoff.NodeID, nodeID, "cutoff")
+	env.Graph().AddEdge(resonance.NodeID, nodeID, "resonance")
 
 	return &value.Gen{
 		NodeID: nodeID,
