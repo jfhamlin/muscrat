33a2efee9196f8814213b27633b290b0590bea1d
James Hamlin <jfhamlin@gmail.com>
2024-01-01 02:04:15 +0800

    Better console gui element
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- pkg/gui/console.go
+++ pkg/gui/console.go
@@ -7,7 +7,9 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
+
 	"github.com/jfhamlin/muscrat/pkg/pubsub"
 )
 
@@ -17,42 +19,51 @@ type (
 	}
 
 	consoleEntry struct {
-		text  string
-		count int
+		text   string
+		count  int
+		object fyne.CanvasObject
 	}
 )
 
 // NewConsole creates a new Console widget.
 func NewConsole() fyne.CanvasObject {
-	history := []consoleEntry{}
-
-	historyText := func() string {
-		sb := strings.Builder{}
-		for _, entry := range history {
-			if entry.count > 0 {
-				sb.WriteString("(")
-				sb.WriteString(fmt.Sprintf("%d", entry.count))
-				sb.WriteString(") ")
-			}
-
-			sb.WriteString(entry.text)
-		}
-		return sb.String()
-	}
-
-	text := canvas.NewText(historyText(), theme.TextColor())
+	history := []*consoleEntry{}
+	entries := container.New(layout.NewVBoxLayout())
 
 	pubsub.Subscribe("console.debug", func(event string, data any) {
 		switch data := data.(type) {
 		case string:
 			if len(history) == 0 || history[len(history)-1].text != data {
-				history = append(history, consoleEntry{text: data})
+				obj := canvas.NewText(data, theme.TextColor())
+				history = append(history, &consoleEntry{
+					text:   data,
+					object: obj,
+				})
+				entries.AddObject(obj)
+				entries.Refresh()
 			} else {
-				history[len(history)-1].count++
+				last := history[len(history)-1]
+				last.inc()
 			}
-			text.Text = historyText()
-			text.Refresh()
 		}
 	})
-	return container.New(nil, text)
+	return entries
+}
+
+func (ce *consoleEntry) String() string {
+	sb := strings.Builder{}
+	if ce.count > 0 {
+		sb.WriteString("(")
+		sb.WriteString(fmt.Sprintf("%d", ce.count))
+		sb.WriteString(") ")
+	}
+
+	sb.WriteString(ce.text)
+	return sb.String()
+}
+
+func (ce *consoleEntry) inc() {
+	ce.count++
+	ce.object.(*canvas.Text).Text = ce.String()
+	ce.object.Refresh()
 }
