6e312a795993b57be6c7b95b3cbe3df90b6218ce
James Hamlin <jfhamlin@gmail.com>
2024-01-02 08:39:23 +0800

    defosc optional params back in
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- pkg/osc/lowfreq.go
+++ pkg/osc/lowfreq.go
@@ -2,19 +2,19 @@ package osc
 
 import "github.com/jfhamlin/muscrat/pkg/ugen"
 
-// NewLFSaw returns a new low-frequency (non-band-limited) sawtooth
+// NewLFSaw returns a new low-frequency (non-band-limited, unipolar) sawtooth
 // oscillator.
 func NewLFSaw(opts ...ugen.Option) ugen.UGen {
 	return New(SamplerFunc(func(phase, dPhase, dutyCycle float64) float64 {
 		phase = dcPhase(phase, dutyCycle)
-		return 2.0*phase - 1.0
+		return phase
 	}), opts...)
 }
 
 func NewLFPulse(opts ...ugen.Option) ugen.UGen {
 	return New(SamplerFunc(func(phase, dPhase, dutyCycle float64) float64 {
 		if phase >= dutyCycle {
-			return -1.0
+			return 0
 		}
 		return 1.0
 	}), opts...)
