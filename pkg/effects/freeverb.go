85cc5b2a55ff82530bf2fd1db9e006acb6cfadf9
James Hamlin <james@goforward.com>
2024-01-28 15:48:42 -0800

    freeverb with defugen
    Signed-off-by: James Hamlin <james@goforward.com>

--- pkg/effects/freeverb.go
+++ pkg/effects/freeverb.go
@@ -5,15 +5,25 @@ import (
 	"math"
 
 	"github.com/jfhamlin/freeverb-go"
+	"github.com/jfhamlin/muscrat/pkg/conf"
 	"github.com/jfhamlin/muscrat/pkg/ugen"
 )
 
 func NewFreeverb(revmod *freeverb.RevModel) ugen.UGen {
+	input32 := make([]float32, conf.BufferSize)
+	outputLeft := make([]float32, conf.BufferSize)
+	outputRight := make([]float32, conf.BufferSize)
+
 	return ugen.UGenFunc(func(ctx context.Context, cfg ugen.SampleConfig, out []float64) {
 		n := len(out)
-		input32 := make([]float32, n)
+		if len(input32) != n {
+			input32 = make([]float32, n)
+			outputLeft = make([]float32, n)
+			outputRight = make([]float32, n)
+		}
+
 		for i := range input32 {
-			input32[i] = float32(cfg.InputSamples["$0"][i])
+			input32[i] = float32(cfg.InputSamples["in"][i])
 		}
 		if roomSizes, ok := cfg.InputSamples["room-size"]; ok {
 			rs := roomSizes[0]
@@ -22,17 +32,24 @@ func NewFreeverb(revmod *freeverb.RevModel) ugen.UGen {
 				revmod.SetRoomSize(roomSize)
 			}
 		}
-		if drys, ok := cfg.InputSamples["dry"]; ok {
-			d := drys[0]
-			dry := float32(math.Max(0, math.Min(1, d)))
+		if damps, ok := cfg.InputSamples["damp"]; ok {
+			dmp := damps[0]
+			damp := float32(math.Max(0, math.Min(1, dmp)))
+			if math.Abs(float64(damp-revmod.GetDamp())) > 0.01 {
+				revmod.SetDamp(damp)
+			}
+		}
+		if mixes, ok := cfg.InputSamples["mix"]; ok {
+			mix := math.Max(0, math.Min(1, mixes[0])) // dry/wet
+			dry := float32(1 - mix)
+			wet := float32(mix)
 			if math.Abs(float64(dry-revmod.GetDry())) > 0.01 {
 				revmod.SetDry(dry)
+				revmod.SetWet(wet)
 			}
 		}
 
-		// TODO: mono and float64 freeverb, implement without allocating
-		outputLeft := make([]float32, n)
-		outputRight := make([]float32, n)
+		// TODO: multi-channel output
 		revmod.ProcessReplace(input32, input32, outputLeft, outputRight, n, 1)
 		for i := range out {
 			out[i] = 0.5 * (float64(outputLeft[i]) + float64(outputRight[i]))
