68433a05be8a48646ec6c74536a7e6d6fd531dc2
James Hamlin <jfhamlin@gmail.com>
2024-09-14 11:21:21 -0700

    Add wails3 files
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- scripts/make/app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# universal fails to build
-wails build -platform darwin/arm64 -tags static -ldflags "-s -w -linkmode external"
-cp -r data/samples build/bin/muscrat.app/Contents/Resources/samples
